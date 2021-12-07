6bbcacbe23650f92eb1a84e38682c5040836fbbe
ilyabill <bilarusilya@gmail.com>
2021-12-07 17:27:03 +0300

    Initial Commit

---
+++ rectangles_constrains_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rectangles_constrains_1
+//
+//  Created by ILYA BILARUS on 07.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
